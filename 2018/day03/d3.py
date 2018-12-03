f74d9d491a4a05ccf89161d7ea2033e187c082db
Justin Bennett <justin.m.bennett@lmco.com>
2018-12-03 11:22:12 -0500

    Initial python solution for 2018 day 3

---
+++ 2018/day03/d3.py
@@ -0,0 +1,25 @@
+# Day 3
+
+f = open("input.txt")
+lines = [x.split() for x in f.readlines()]
+f.close()
+test = [x.split() for x in "#1 @ 1,3: 4x4\n#2 @ 1,3: 4x4\n#3 @ 1,3: 2x2".split("\n")]
+
+points = dict()
+for x in lines:
+    points[x[0][1:]] = [tuple(x[2][:-1].split(',')), tuple(x[3].split('x'))]
+
+# Part 1
+grid = dict()
+for id, claim in points.items():
+    p = claim[0]
+    dim = claim[1]
+    for x in range(int(dim[0])):
+        for y in range(int(dim[1])):
+            new_x = int(p[0]) + x
+            new_y = int(p[1]) + y
+            if (new_x, new_y) not in grid:
+                grid[new_x, new_y] = 1
+            else:
+                grid[new_x, new_y] += 1
+print(len([k for k,v in grid.items() if v > 1]))
