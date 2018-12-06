191f177ef25c58695796632241a0725aff2e84ae
Justin Bennett <justin.m.bennett@lmco.com>
2018-12-06 14:05:14 -0500

    Finished day 5, added empty day 6

--- 2018/day05/d5.py
+++ 2018/day05/d5.py
@@ -1,22 +1,32 @@
 # Day 5
 
+import string
+
 f = open('input.txt')
-p = list(f.readlines()[0].strip())
+inp = list(f.readlines()[0].strip())
 
 test = list("dabAcCaCBAcCcaDA")
-#p = test
+
+def react(p):
+    i = 0
+    while i < len(p)-1:
+        if p[i].islower() and p[i+1].lower() == p[i] and p[i+1].isupper() or p[i].isupper() and p[i+1].upper() == p[i] and p[i+1].islower():
+            p.pop(i)
+            p.pop(i)
+            i = i - 1
+            if i < 0: i = 0
+        else:
+            i += 1
+    return len(p)
 
 # Part 1
-i = 0
-while i < len(p)-1:
-    if p[i].islower() and p[i+1].lower() == p[i] and p[i+1].isupper() or p[i].isupper() and p[i+1].upper() == p[i] and p[i+1].islower():
+print(react(inp.copy()))
 
-        p.pop(i)
-        p.pop(i)
-        i = i - 1
-        if i < 0: i = 0
-    else:
-        i += 1
+# Part 2
+results = dict()
+for unit in string.ascii_lowercase:
+    both = [unit, unit.upper()]
+    reduced = [x for x in inp if x not in both]
+    results[unit] = react(reduced)
 
-print(len(p))
-    
+print(results[min(results, key=results.get)])
