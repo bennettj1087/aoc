6a95e3640f132c737c53695a7da51452aede53b1
Justin Bennett <bennettj1087@gmail.com>
2022-12-01 21:14:42 -0500

    day 1

--- aocjmb/aoc2022/day1.py
+++ aocjmb/aoc2022/day1.py
@@ -1,26 +1,40 @@
 from aocd import data
 from itertools import combinations
 
+def calc_sums(data):
+    elves = [x.split("\n") for x in data.strip().split("\n\n")]
+    for x in range(len(elves)):
+        elves[x] = [int(y) for y  in elves[x]]
+    sums = [sum(x) for x in elves]
+    return sums
 
 def part_a(data):
-    print("help")
+    return max(calc_sums(data))
 
 
 def part_b(data):
-    print("help")
+    return sum(sorted(calc_sums(data))[-3:])
 
 
 test_data = """\
-1721
-979
-366
-299
-675
-1456
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
 """
 
 if __name__ == "__main__":
-    assert part_a(test_data) == 514579
-    assert part_b(test_data) == 241861950
+    assert part_a(test_data) == 24000
+    assert part_b(test_data) == 45000
     print(part_a(data))
     print(part_b(data))
